083b91e84bbc8bbe74584c9a4cf512adabbd7d2a
m.sinans <msomercikoglu@gmail.com>
2023-03-13 14:13:33 +0300

    Add project files.

---
+++ Constructor(Yapici)_Metot/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructor_Yapici_Metot
+{
+    internal class Ogrenci
+    {
+        public Ogrenci()
+        {
+            Console.WriteLine("Adı: Ali");
+            Console.WriteLine("Soyadı: Can");
+            Console.WriteLine("Meslek: Öğrenci");
+        }
+    }
+}
---
+++ Constructor(Yapici)_Metot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Yapici_Metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+
+            Console.Read();
+        }
+    }
+}
